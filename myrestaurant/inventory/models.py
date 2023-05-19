d5306a072029afcc5b9943cb190fc5913c36520d
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2023-05-20 00:19:47 +0700

    Models config: Ingredient models

--- myrestaurant/inventory/models.py
+++ myrestaurant/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=10)
+    price_per_unit = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    
+    def __str__(self):
+        return f"""
+        name={self.name};
+        qty={self.quantity};
+        unit={self.unit};
+        unit_price={self.price_per_unit}
+        """
+
