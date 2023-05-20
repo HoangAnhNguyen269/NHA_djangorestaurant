0d18696fb122a1123e19f41a1fe35a5e9048be07
HoangAnhNguyen269 <74226464+HoangAnhNguyen269@users.noreply.github.com>
2023-05-20 15:17:15 +0700

    Views: Home View; no style yet

--- myrestaurant/inventory/views.py
+++ myrestaurant/inventory/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+#import Views
+from django.views.generic import TemplateView
+#import models
+from .models import Ingredient, MenuItem, RecipeRequirements, Purchase
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'inventory/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ingredients'] = Ingredient.objects.all()
+        context['menu_items'] = MenuItem.objects.all()
+        context['purchases'] = Purchase.objects.all()
+        return context
+    
